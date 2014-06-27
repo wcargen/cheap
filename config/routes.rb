db2f416cdb554637ed4722d82a8e1290c0774852
wcargen <wcargen@gmail.com>
2014-06-27 18:41:33 -0500

    Remove Contact page.  Add slogan to home page

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
