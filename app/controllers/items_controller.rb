6e111cde0acef33c2868afa81c970fd946a75427
wcargen <wcargen@gmail.com>
2014-06-27 13:04:18 -0500

    Add rough version of search results

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,15 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @food = params[:food]
+    @postal_code = params[:postal_code]
+    @price__lte = params[:price__lte]
+
+    request = Typhoeus::Request.new(
+      "http://api.locu.com/v1_0/menu_item/search/?",
+      params: {name: @food, postal_code: @postal_code, price__lte: @price__lte,
+               api_key: Rails.application.secrets.locu_api_key}).run
+      @result = JSON.parse(request.body)
   end
 
   def create
