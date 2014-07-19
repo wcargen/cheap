615d4d2680c6c4d0233205801757ee39259fb5d2
wcargen <wcargen@gmail.com>
2014-07-19 15:07:40 -0400

    Fix javascript error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,36 +17,3 @@
 //= require leaflet
 //= require_tree .
 
-$(document).ready(function() {
-
-// choose text for the show/hide link - can contain HTML (e.g. an image)
-var showText='[?]';
-var hideText='[-]';
-
-// initialise the visibility check
-var is_visible = false;
-
-// append show/hide links to the element directly preceding the element with a class of "toggle"
-$('.toggle').prev().append(' <a href="#" class="toggleLink">'+showText+'</a>');
-
-// hide all of the elements with a class of 'toggle'
-$('.toggle').hide();
-
-// capture clicks on the toggle links
-$('a.toggleLink').click(function() {
-
-// switch visibility
-is_visible = !is_visible;
-
-// change the link depending on whether the element is shown or hidden
-$(this).html( (!is_visible) ? showText : hideText);
-
-// toggle the display - uncomment the next line for a basic "accordion" style
-// $('.toggle').hide();$('a.toggleLink').html(showText);
-$(this).parent().next('.toggle').toggle('slow');
-
-// return false so any link destination is not followed
-return false;
-
-});
-});
