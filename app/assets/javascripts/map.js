0cd47ba12a9d1339480db385e98871df5c398dc7
wcargen <wcargen@gmail.com>
2014-08-04 20:26:03 -0400

    Change maxZoom for map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -5,7 +5,7 @@ window.onload = function() {
 
   L.tileLayer('http://{s}.tiles.mapbox.com/v3/wcargen.j0dp52nf/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 15
+    maxZoom: 18
   }).addTo(map);
 
   var data = {
